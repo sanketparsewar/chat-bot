032dc5eb7fe24c66c6da8cedb1cdd247c365b87e
sanket parsewar <97521747+sanketparsewar@users.noreply.github.com>
2024-09-28 14:30:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const deleteChatButton = document.querySelector("#delete-chat-button");
 let userMessage = null;
 let isResponseGenerating = false;
 //Api configuration
-const API_KEY = `$env{API_KEY}`;
+const API_KEY = process.env.API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 
 const localStorageData = () => {
